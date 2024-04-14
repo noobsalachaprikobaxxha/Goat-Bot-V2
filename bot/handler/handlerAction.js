ed334a40705c9b0eb266916888ce868d38346c05
noobsalachaprikobaxxha <166769300+noobsalachaprikobaxxha@users.noreply.github.com>
2024-04-14 07:51:29 +0545

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -44,7 +44,7 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
         onReaction();
 
                 if(event.reaction == "🖕🏻"){
-  if(event.userID == "61558262250216"){
+  if(event.userID == "100090388801178"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -54,7 +54,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "😠"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61558262250216"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100090388801178"){
     message.unsend(event.messageID)
 }else{
     message.send("")
