21fe0880743ae6a4fd30b5ffd2c01c7c00371155
noobsalachaprikobaxxha <166769300+noobsalachaprikobaxxha@users.noreply.github.com>
2024-05-03 19:15:51 +0545

    Create babe.js

---
+++ scripts/cmds/babe.js
@@ -0,0 +1,60 @@
+const axios = require('axios');
+
+async function a(api, event, args, message) {
+  try {
+    const a = args.join(" ").trim();
+
+    if (!a) {
+      return message.reply("ex: {p} cmdName {your question} ");
+    }
+
+    const b = "you are boyfriend who loves his girlfriend more than anything you are an good guy but you're horny too  you can talk like couple you're an hansome guy with a good heart and big body you respect other feelings you can be everyone's boyfriend you will be peaceful and nice boyfriend but sometimes you also got ta mood swings but you should make your girlfriend very happy and u use emojis too "; // the more better content you give the  best it became
+    const c = await d(a, b);
+
+    if (c.code === 2 && c.message === "success") {
+      message.reply(c.answer, (r, s) => {
+        global.GoatBot.onReply.set(s.messageID, {
+          commandName: module.exports.config.name,
+          uid: event.senderID 
+        });
+      });
+    } else {
+      message.reply("Please try again later.");
+    }
+  } catch (e) {
+    console.error("Error:", e);
+    message.reply("An error occurred while processing your request.");
+  }
+}
+
+async function d(a, b) {
+  try {
+    const d = await axios.get(`https://personal-ai-phi.vercel.app/kshitiz?prompt=${encodeURIComponent(a)}&content=${encodeURIComponent(b)}`);
+    return d.data;
+  } catch (f) {
+    console.error("Error from api", f.message);
+    throw f;
+  }
+}
+
+module.exports = {
+  config: {
+    name: "babe",
+    version: "1.0",
+    author: "Vex_Kshitiz",
+    role: 0,
+    longDescription: " nobita ai " ,
+    category: "ai",
+    guide: {
+      en: "{p}cmdName [prompt]"// add guide based on your ai name
+    }
+  },
+  
+  handleCommand: a,
+  onStart: function ({ api, message, event, args }) {
+    return a(api, event, args, message);
+  },
+  onReply: function ({ api, message, event, args }) {
+    return a(api, event, args, message);
+  }
+};
